d9fc979cf0a0f2fcb064508772e18c95bfc71036
Ivan Kniazev <i.kniazev@seventest.ru>
2024-10-31 23:47:02 +0300

    clean the trash

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,13 +1,10 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
 
-import java.util.List;
 
 public class Main {
     private static final UserService userService = new UserServiceImpl();
